270fb65618ec2d0cd8f7a0ec9e5d20b473f43c46
Logan Negley <logannegley@gmail.com>
2020-02-27 20:26:08 -0600

    building form

---
+++ my-app/src/components/Form.js
@@ -0,0 +1,20 @@
+import React, {useReducer} from 'react';
+
+
+function Form() {
+
+  return (
+    <div className="form-container">
+        <form>
+            <input 
+                type='text'
+                name='todo'
+            ></input>
+            <button>Add Todo</button>
+            <button>Clear Completed</button>
+        </form>
+    </div>
+  );
+}
+
+export default Form;
