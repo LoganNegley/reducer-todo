1297de3257a2598d64e35af34d50622d5bad9456
Logan Negley <logannegley@gmail.com>
2020-02-27 20:27:59 -0600

    form rendered to app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, {useReducer} from 'react';
 import './App.css';
 import {initialState, todoReducer} from '../src/reducer/index';
 import TodoList from '../src/components/TodoList';
+import Form from '../src/components/Form';
 
 function App() {
 
@@ -9,6 +10,7 @@ const [state, dispatch] = useReducer(todoReducer,initialState)
 
   return (
     <div className="App">
+    <Form/>
     <TodoList state={state}/>
     </div>
   );
