1ea9f4d76da9320bce84b5d5fdb8559c9b8f8428
Logan Negley <logannegley@gmail.com>
2020-02-27 19:58:55 -0600

    initial sate set up

---
+++ my-app/src/reducer/index.js
@@ -0,0 +1,12 @@
+export const initialState ={
+    list:[
+        { item: 'Learn React', 
+        completed:false, 
+        id:384521489451 },
+
+        { item:'Learn Redux', 
+        completed:false,
+        id:3548710235 }
+    ] }
+
+    
